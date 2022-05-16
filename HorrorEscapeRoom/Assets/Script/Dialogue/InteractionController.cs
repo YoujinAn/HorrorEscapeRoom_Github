844967d4f8adbf6a0e214174874ffe76448e6b15
shatiel98 <shatiel98@gmail.com>
2022-05-15 17:30:42 -0700

    bug fix and add Odin

--- HorrorEscapeRoom/Assets/Script/Dialogue/InteractionController.cs
+++ HorrorEscapeRoom/Assets/Script/Dialogue/InteractionController.cs
@@ -5,33 +5,3 @@ using UnityEngine;
 public class InteractionController : MonoBehaviour
 {
 }
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum INTERACTION
-{
-    DOOR,
-    CLOSET,
-    DESK,
-    ITEM
-}
-
-public class InteractionController : MonoBehaviour
-{
-}
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum INTERACTION
-{
-    DOOR,
-    CLOSET,
-    DESK,
-    ITEM
-}
-
-public class InteractionController : MonoBehaviour
-{
-}
